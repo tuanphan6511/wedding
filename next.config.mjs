2a6514acf0c6c3ce15aaa821d7833d8ab4b42d6c
Tuấn Phan <99529590+tuanphan6511@users.noreply.github.com>
2025-05-24 22:54:35 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  assetPrefix: '/wedding-landing',
+  basePath: '/wedding-landing',
+};
+
+export default nextConfig;
+
